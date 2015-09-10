56fc15d050d5c2ee410ea5847d966f03c9462e7c
Benedict(이호정) <benedict.lee@daumkakao.com>
2015-09-10 14:27:50 +0900

    initial commit

---
+++ buildtest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  buildtest
+//
+//  Created by hojeonglee on 2015. 9. 10..
+//  Copyright (c) 2015년 dk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
